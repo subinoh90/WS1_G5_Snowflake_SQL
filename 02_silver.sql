8d25de8f76653878ed806601289ece069e12e209
Subin Oh <spreadmercy@gmail.com>
2025-10-22 17:37:10 +1100

    update: refined Silver layer SQL (data cleaning and validation fixes)

--- 02_silver.sql
+++ 02_silver.sql
@@ -59,7 +59,7 @@ INSERT INTO customer_products
 SELECT 
     TRIM(ProductID), 
     LOWER(TRIM(productType)) AS productType,
-    TRIM(productName) AS productName,
+    LOWER(TRIM(productName)) AS productName,
     COALESCE(
         TRY_TO_DATE(acquisitionDate, 'YYYY-MM-DD'),
         TRY_TO_DATE(acquisitionDate, 'YYYY/MM/DD'),
@@ -100,7 +100,6 @@ FROM (
 ) t
 WHERE rn = 1;
 
-SELECT * FROM bronze.customer_products;
 INSERT INTO campaign_interactions
 SELECT 
     TRIM(customer_id),
@@ -137,125 +136,204 @@ FROM (
 ) t
 WHERE rn = 1;
 
-SELECT * FROM campaign_interactions;
-
--- 4) Remove outlier data
--- See the visualization in Snowflake to detect the outlier and remove them
+-- 4) Clean data in customer_products table
+-- See the visualization in Snowflake
 SELECT * FROM customer_products;
 
+-- Remove the rows that product ID missing or error
 DELETE FROM customer_products
-WHERE total_account_balance = 999999 OR
-total_liabilities = 123456 OR
-income = 426863;
+WHERE product_id IS NULL
+   OR LOWER(product_id) IN ('undefined', 'nan', 'error')
+   OR TRY_TO_NUMBER(product_id) IS NOT NULL;
 
-SELECT * FROM customer_products;
+-- Remove existing columns with missing or invalid data
+-- These columns contain inconsistent or unreliable values, so we drop them to ensure data integrity
+ALTER TABLE customer_products DROP COLUMN product_type, product_name;
 
--- 5) Handle missing data
--- 5.1) customer_products table
--- See the visualization in Snowflake to detect missing data
-SELECT * FROM customer_products;
--- In customer_product, there are missing data in preferred_channel, city, gender, income, join_date, and marital_status, which are the customer's profile data
+-- Add new columns to replace the dropped ones
+ALTER TABLE customer_products
+ADD COLUMN product_type VARCHAR, product_name VARCHAR;
 
--- If the same customer has other rows with a known data, use that value to fill in the missing one
--- preferred_channel
-UPDATE customer_products cp
-SET preferred_channel = (
-    SELECT MAX(preferred_channel)
-    FROM customer_products
-    WHERE customer_id = cp.customer_id
-      AND preferred_channel IS NOT NULL
-)
-WHERE cp.preferred_channel IS NULL;
-
--- city
-UPDATE customer_products cp
-SET city = (
-    SELECT MAX(city)
-    FROM customer_products
-    WHERE customer_id = cp.customer_id
-      AND city IS NOT NULL
-)
-WHERE cp.city IS NULL;
-
--- gender
-UPDATE customer_products cp
-SET gender = (
-    SELECT MAX(gender)
-    FROM customer_products
-    WHERE customer_id = cp.customer_id
-      AND gender IS NOT NULL
-)
-WHERE cp.gender IS NULL;
-
--- income
-UPDATE customer_products cp
-SET income = (
-    SELECT ROUND(AVG(income), 2)
-    FROM customer_products
-    WHERE customer_id = cp.customer_id
-      AND income IS NOT NULL
-)
-WHERE cp.income IS NULL;
-
--- join_date
-UPDATE customer_products cp
-SET join_date = (
-    SELECT MAX(join_date)
-    FROM customer_products
-    WHERE customer_id = cp.customer_id
-      AND join_date IS NOT NULL
-)
-WHERE cp.join_date IS NULL;
-
--- marital_status
-UPDATE customer_products cp
-SET marital_status = (
-    SELECT MAX(marital_status)
-    FROM customer_products
-    WHERE customer_id = cp.customer_id
-      AND marital_status IS NOT NULL
-)
-WHERE cp.marital_status IS NULL;
-
--- The other missing preferred_channel are filled wiht the most common channel
+-- Populate the new columns using CASE logic based on product_id
+-- This ensures each product_id is mapped to its correct type and name, restoring consistency
 UPDATE customer_products
-SET preferred_channel = 'phone call'
-WHERE preferred_channel IS NULL;
-
--- The other missing gender are filled with 'x' (not specified)
-UPDATE customer_products
-SET gender = 'x'
-WHERE gender IS NULL;
-
---the other missing income are filled with average income of other rows that share the same product_type, product_name, and marital_status.
-UPDATE customer_products cp
-SET income = (
-    SELECT ROUND(AVG(income), 2)
-    FROM customer_products
-    WHERE product_type = cp.product_type
-      AND product_name = cp.product_name
-      AND income IS NOT NULL
-)
-WHERE cp.income IS NULL;
-
--- The remaining missing values in city, join_date, and marital_status are left as NULL
--- These attributes cannot be reliably imputed using statistical methods
-SELECT * FROM customer_products;
-
--- 5.2) campaign_interactions
--- See the visualization in Snowflake to detect missing data
-SELECT * FROM campaign_interactions;
--- In campaign_interactions, there are missing data in click_status
-
--- Set click_status to 'yes' if click_date is present, 'no' if click_date is missing
-UPDATE campaign_interactions
-SET click_status = CASE
-    WHEN click_date IS NOT NULL THEN 'yes'
-    WHEN click_date IS NULL THEN 'no'
+SET product_type = CASE product_id
+    WHEN 'P001' THEN 'term deposit'
+    WHEN 'P002' THEN 'savings account'
+    WHEN 'P003' THEN 'term credit card'
+    WHEN 'P004' THEN 'term home loan'
+    WHEN 'P005' THEN 'term premium account'
+END,
+product_name = CASE product_id
+    WHEN 'P001' THEN 'saver premium'
+    WHEN 'P002' THEN 'saver basic'
+    WHEN 'P003' THEN 'rewards plus'
+    WHEN 'P004' THEN 'variable home loan'
+    WHEN 'P005' THEN 'membership premium'
 END;
 
--- 5.3) campaigns
--- See the visualization in Snowflake to detect missing data
+-- Update customer_id to NULL if it doesn't match the pattern 'C' followed by exactly three digits
+UPDATE customer_products
+SET customer_id = NULL
+WHERE customer_id IS NOT NULL
+  AND customer_id NOT RLIKE '^C[0-9]{3}$';
+
+-- Update rows with NULL customer_id by matching on customer_name and date_of_birth
+UPDATE customer_products t1
+SET t1.customer_id = t2.customer_id
+FROM customer_products t2
+WHERE t1.customer_id IS NULL
+AND UPPER(t1.customer_name) = UPPER(t2.customer_name)
+AND t1.date_of_birth = t2.date_of_birth
+AND t2.customer_id IS NOT NULL;
+
+-- Remove rows that still have NULL customer_id after imputation
+DELETE FROM customer_products
+WHERE customer_id IS NULL;
+
+-- Replace known invalid customer's infomation values with NULL to improve data quality
+-- Create a reference table
+CREATE OR REPLACE TEMP TABLE invalid_values (value STRING);
+INSERT INTO invalid_values VALUES
+  ('nan'), ('999999999'), ('-9999'), ('???'), ('undefined'), ('NaN'), ('unknown'), ('other'), ('error');
+
+-- Use it to update values
+UPDATE customer_products
+SET customer_name = NULL
+WHERE LOWER(customer_name) IN (SELECT value FROM invalid_values);
+UPDATE customer_products
+SET preferred_channel = NULL
+WHERE LOWER(preferred_channel) IN (SELECT value FROM invalid_values);
+UPDATE customer_products
+SET city = NULL
+WHERE LOWER(city) IN (SELECT value FROM invalid_values);
+UPDATE customer_products
+SET gender = NULL
+WHERE LOWER(gender) IN (SELECT value FROM invalid_values);
+UPDATE customer_products
+SET marital_status = NULL
+WHERE LOWER(marital_status) IN (SELECT value FROM invalid_values);
+
+-- Update rows with NULL customer_name by matching on customer_id
+UPDATE customer_products t1
+SET t1.customer_name = t2.customer_name
+FROM customer_products t2
+WHERE t1.customer_name IS NULL
+AND t1.customer_id = t2.customer_id
+AND t2.customer_name IS NOT NULL;
+
+-- Update rows with NULL preferred_channel by matching on customer_id
+UPDATE customer_products t1
+SET t1.preferred_channel = t2.preferred_channel
+FROM customer_products t2
+WHERE t1.preferred_channel IS NULL
+AND t1.customer_id = t2.customer_id
+AND t2.preferred_channel IS NOT NULL;
+
+-- Replace NULL preferred_channel with the most frequent non-null value
+UPDATE customer_products
+SET preferred_channel = (
+  SELECT preferred_channel
+  FROM customer_products
+  WHERE preferred_channel IS NOT NULL
+  GROUP BY preferred_channel
+  ORDER BY COUNT(*) DESC
+  LIMIT 1
+)
+WHERE preferred_channel IS NULL;
+
+-- Update rows with NULL city by matching on customer_id
+UPDATE customer_products t1
+SET t1.city = t2.city
+FROM customer_products t2
+WHERE t1.city IS NULL
+AND t1.customer_id = t2.customer_id
+AND t2.city IS NOT NULL;
+
+-- Update rows with NULL gender by matching on customer_id
+UPDATE customer_products t1
+SET t1.gender = t2.gender
+FROM customer_products t2
+WHERE t1.gender IS NULL
+AND t1.customer_id = t2.customer_id
+AND t2.gender IS NOT NULL;
+
+-- Update rows with NULL marital_status by matching on customer_id
+UPDATE customer_products t1
+SET t1.marital_status = t2.marital_status
+FROM customer_products t2
+WHERE t1.marital_status IS NULL
+AND t1.customer_id = t2.customer_id
+AND t2.marital_status IS NOT NULL;
+
+-- Remove rows with invalid total_account_balance
+DELETE FROM customer_products
+WHERE total_account_balance = -9999;
+
+-- Remove rows with invalid total_liabilities values
+DELETE FROM customer_products
+WHERE total_liabilities IN (-9999, 999999999);
+
+SELECT * FROM customer_products;
+
+-- 5) Clean data in campaigns table
+-- See the visualization in Snowflake
 SELECT * FROM campaigns;
--- In campaigns, there are missing data in budget, but it cannot be reliably imputed using statistical methods
--- so, leave them as NULL
+
+-- Fill missing campaign data to preserve completeness for analysis and avoid loss from small sample size.
+-- Update launch_date for campaign CC2025
+UPDATE campaigns
+SET launch_date = '2025-01-05'
+WHERE campaign_id = 'CC2025';
+
+-- Update launch_date for campaign SAV2025
+UPDATE campaigns
+SET launch_date = '2024-12-25'
+WHERE campaign_id = 'SAV2025';
+
+-- Update budget and channel for campaign UPG2025
+UPDATE campaigns
+SET budget = 57839.00,
+    channel = 'app, sms'
+WHERE campaign_id = 'UPG2025';
+
+-- 6) Clean data in campaign_interactions table
+-- See the visualization in Snowflake
+SELECT * FROM campaign_interactions;
+
+-- Replace known invalid values with NULL to improve data quality
+UPDATE campaign_interactions
+SET customer_id = NULL
+WHERE LOWER(customer_id) IN (SELECT value FROM invalid_values);
+UPDATE campaign_interactions
+SET campaign_id = NULL
+WHERE LOWER(campaign_id) IN (SELECT value FROM invalid_values);
+UPDATE campaign_interactions
+SET promotion_type = NULL
+WHERE LOWER(promotion_type) IN (SELECT value FROM invalid_values);
+UPDATE campaign_interactions
+SET click_status = NULL
+WHERE LOWER(click_status) IN (SELECT value FROM invalid_values);
+
+-- Remove rows with missing customer_id or campaign_id
+DELETE FROM campaign_interactions
+WHERE customer_id IS NULL
+   OR campaign_id IS NULL;
+
+-- Update NULL promotion_type using the most common value per campaign_id
+UPDATE campaign_interactions AS ci
+SET promotion_type = mode.promotion_type
+FROM (
+  SELECT campaign_id, promotion_type
+  FROM (
+    SELECT campaign_id, promotion_type,
+           COUNT(*) AS freq,
+           ROW_NUMBER() OVER (PARTITION BY campaign_id ORDER BY COUNT(*) DESC) AS rn
+    FROM campaign_interactions
+    WHERE promotion_type IS NOT NULL
+    GROUP BY campaign_id, promotion_type
+  )
+  WHERE rn = 1
+) AS mode
+WHERE ci.promotion_type IS NULL
+  AND ci.campaign_id = mode.campaign_id;
